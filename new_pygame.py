516cb7deb1f839dd69fd4964ba2aafeb237b0e60
AntsThatWander <anubspider@gmail.com>
2019-12-05 23:21:35 +0900

    resources_changed

--- new_pygame.py
+++ new_pygame.py
@@ -1,14 +1,19 @@
 import pygame
+
 #초기화
 pygame.init()
-width, height = 1200, 900
+width, height = 1400, 1030
 screen = pygame.display.set_mode((width, height))
 
 #이미지를 가져온다
-player = pygame.image.load("resources/images/dude.png")
-player_stick = pygame.image.load("resources/Black_lord.png")
-red = pygame.image.load("resources/my_resources/Test_Red.png")
-
+player_stick = pygame.image.load("resources/my_resources/player_ready_Red.png")
+ball_room = pygame.image.load("resources/my_resources/magic_room.png")
+card_room = pygame.image.load("resources/my_resources/card_room.png")
+pave_road = pygame.image.load("resources/my_resources/pave_road.png")
+ground = pygame.image.load("resources/my_resources/ground.png")
+tent = pygame.image.load("resources/my_resources/tent.png")
+tent_road = pygame.image.load("resources/my_resources/tent_road.png")
+castle = pygame.image.load("resources/my_resources/castle_wall.png")
 
 #화면 띄우기
 while True:
@@ -16,10 +21,17 @@ while True:
     screen.fill((255,255,255)) #RGB
 
     #모든 요소들을 다시 그린다.
-    
-    screen.blit(player, (0,100)) #blit은 다시 그리기
-    screen.blit(player_stick, (100,300))
-    screen.blit(red, (0,600))
+    #blit은 다시 그리기
+    for x in range(5):
+            screen.blit(tent_road,(0,(x+1)*140-110))
+            screen.blit(tent,(0,(x+1)*140-110))
+            screen.blit(pave_road,(300,(x+1)*140-110))
+            screen.blit(ground,(300,(x+1)*140+20))
+    screen.blit(castle, (200,30))
+    screen.blit(ball_room, (0,730))
+    screen.blit(card_room, (300,730))
+    screen.blit(player_stick, (220,310))
+   
 
     #화면을 다시 그린다.
     pygame.display.flip() 
