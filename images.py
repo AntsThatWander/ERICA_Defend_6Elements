bb4af7de039ad71c3acd3552678853be74bfde4b
AntsThatWander <anubspider@gmail.com>
2019-12-10 23:55:57 +0900

    make code clear using dictionary

--- images.py
+++ images.py
@@ -1,12 +1,73 @@
 import pygame
-#이미지를 가져온다
-red = pygame.image.load("resources/my_resources/player_ready_Red.png")
-green = pygame.image.load("resources/my_resources/player_ready_Green.png")
-blue = pygame.image.load("resources/my_resources/player_ready_Blue.png")
-yellow = pygame.image.load("resources/my_resources/player_ready_Yellow.png")
-purple = pygame.image.load("resources/my_resources/player_ready_Purple.png")
-jade = pygame.image.load("resources/my_resources/player_ready_Jade.png")
-fin = pygame.image.load("resources/my_resources/player_shoot.png")
+#이미지를 가져온다. 나중에 클래스로 묶도록 한다. Load images
+def Lv0_monster():
+    ling = pygame.image.load("resources/my_resources/nighthorror_ling.png")
+    sting = pygame.image.load("resources/my_resources/sting.png")
+    ball = pygame.image.load("resources/my_resources/cannon_ball.png")
+    return {'ling' : ling, 'sting' : sting, 'ball' : ball}
+def Lv1_monster():
+    zombie = pygame.image.load("resources/my_resources/zombie.png")
+    zombie_barrier = pygame.image.load("resources/my_resources/zombie_barrier.png")
+    return {'zombie' : zombie, 'zombie_barrier' : zombie_barrier}
+def Lv2_monster():
+    nighthorror = pygame.image.load("resources/my_resources/nighthorror.png")
+    crawler = pygame.image.load("resources/my_resources/crawler.png")
+    return {'nighthorror' : nighthorror, 'crawler' : crawler}
+def Lv3_monster():
+    cannon = pygame.image.load("resources/my_resources/cannon.png")
+    seer = pygame.image.load("resources/my_resources/overseer.png")
+    return {'cannon' : cannon, 'seer' : seer}
+def Lv4_monster():
+    black = pygame.image.load("resources/my_resources/Black_lord.png")
+    return {'black' : black}
+def Lv5_monster():
+    final = pygame.image.load("resources/my_resources/Final_lord.png")
+    return {'final' : final}
+
+#플레이어, 환영 그리고 화살 players, mirrors and arrows
+def player():
+    red = pygame.image.load("resources/my_resources/player_ready_Red.png")
+    green = pygame.image.load("resources/my_resources/player_ready_Green.png")
+    blue = pygame.image.load("resources/my_resources/player_ready_Blue.png")
+    yellow = pygame.image.load("resources/my_resources/player_ready_Yellow.png")
+    purple = pygame.image.load("resources/my_resources/player_ready_Purple.png")
+    jade = pygame.image.load("resources/my_resources/player_ready_Jade.png")
+    fin = pygame.image.load("resources/my_resources/player_shoot.png")
+    return {'red' : red, 'green' : green, 'blue' : blue, 'yellow' : yellow, 'purple' : purple, 'jade' : jade, 'shoot' : fin} 
+def mirror():
+    red = pygame.image.load("resources/my_resources/mirror_ready_Red.png")
+    green = pygame.image.load("resources/my_resources/mirror_ready_Green.png")
+    blue = pygame.image.load("resources/my_resources/mirror_ready_Blue.png")
+    yellow = pygame.image.load("resources/my_resources/mirror_ready_Yellow.png")
+    purple = pygame.image.load("resources/my_resources/mirror_ready_Purple.png")
+    jade = pygame.image.load("resources/my_resources/mirror_ready_Jade.png")
+    fin = pygame.image.load("resources/my_resources/mirror_shoot.png")
+    return {'red' : red, 'green' : green, 'blue' : blue, 'yellow' : yellow, 'purple' : purple, 'jade' : jade, 'shoot' : fin}
+def arrow():
+    red = pygame.image.load("resources/my_resources/arrow_Red.png")
+    green = pygame.image.load("resources/my_resources/arrow_Green.png")
+    blue = pygame.image.load("resources/my_resources/arrow_Blue.png")
+    yellow = pygame.image.load("resources/my_resources/arrow_Yellow.png")
+    purple = pygame.image.load("resources/my_resources/arrow_Purple.png")
+    jade = pygame.image.load("resources/my_resources/arrow_Jade.png")
+    return {'red' : red, 'green' : green, 'blue' : blue, 'yellow' : yellow, 'purple' : purple, 'jade' : jade}
+
+#배경 backgrounds
+def backgrounds():
+    return {}
+
+
+#버튼 및 카드 buttons and cards 
+def button():
+    Red_button = pygame.image.load("resources/my_resources/Red_button.png")
+    Green_button = pygame.image.load("resources/my_resources/Green_button.png")
+    Blue_button = pygame.image.load("resources/my_resources/Blue_button.png")
+    Yellow_button = pygame.image.load("resources/my_resources/Yellow_button.png")
+    Purple_button = pygame.image.load("resources/my_resources/Purple_button.png")
+    Jade_button = pygame.image.load("resources/my_resources/Jade_button.png")
+    return {'red' : Red_button, 'green' : Green_button, 'blue' : Blue_button, 'yellow' : Yellow_button, 'purple' : Purple_button, 'jade' : Jade_button}
+    
+
 ball_room = pygame.image.load("resources/my_resources/ball_room.png")
 card_room = pygame.image.load("resources/my_resources/card_room.png")
 pave_road = pygame.image.load("resources/my_resources/pave_road.png")
@@ -14,21 +75,6 @@ tent = pygame.image.load("resources/my_resources/tent.png")
 tent_road = pygame.image.load("resources/my_resources/tent_road.png")
 castle = pygame.image.load("resources/my_resources/castle_wall.png")
 card_Red = pygame.image.load("resources/my_resources/Red_card_1.png")
-zombie = pygame.image.load("resources/my_resources/zombie.png")
-zombie_barrier = pygame.image.load("resources/my_resources/zombie_barrier.png")
-nighthorror = pygame.image.load("resources/my_resources/nighthorror.png")
-ling = pygame.image.load("resources/my_resources/nighthorror_ling.png")
-cannon = pygame.image.load("resources/my_resources/cannon.png")
-ball = pygame.image.load("resources/my_resources/cannon_ball.png")
-crawler = pygame.image.load("resources/my_resources/crawler.png")
-sting = pygame.image.load("resources/my_resources/sting.png")
-seer = pygame.image.load("resources/my_resources/overseer.png")
-black = pygame.image.load("resources/my_resources/Black_lord.png")
-final = pygame.image.load("resources/my_resources/Final_lord.png")
-arrow_Red = pygame.image.load("resources/my_resources/arrow_Red.png")
-Red_button = pygame.image.load("resources/my_resources/Red_button.png")
-Green_button = pygame.image.load("resources/my_resources/Green_button.png")
-Blue_button = pygame.image.load("resources/my_resources/Blue_button.png")
-Yellow_button = pygame.image.load("resources/my_resources/Yellow_button.png")
-Purple_button = pygame.image.load("resources/my_resources/Purple_button.png")
-Jade_button = pygame.image.load("resources/my_resources/Jade_button.png")
+
+
+
--- new_pygame.py
+++ new_pygame.py
@@ -6,22 +6,47 @@ pygame.init()
 width, height = 1280, 960
 screen = pygame.display.set_mode((width, height))
 
-#플레이어 이동 리스트
+#플레이어 위치 player location
 keys = [False, False]
 playpos = [220, 310]
-#플레이어 버튼 리스트
-button = [images.Red_button, images.Green_button,images.Blue_button,images.Yellow_button,images.Purple_button,images.Jade_button]
-active_button = button[0]
-#플레이어 이미지 리스트
-player = [images.red, images.green, images.blue, images.yellow, images.purple, images.jade]
-active_player = player[0]
+#버튼 사전 button dictionary
+button = images.button()
+#플레이어 이미지 사전 plyaer dictionary
+player = images.player()
+pshoot = player['shoot']
+#환영 이미지 사전 mirror dictionary
+mirror = images.mirror()
+mshoot = player['shoot']
+#화살 이미지 사전 arrow images dictionary
+arrow = images.arrow()
+#현재 이미지 값 current image color
+cur_color = 'red'
+
 #
 acc = [0,0]
 #화살
 arrows = []
+#레벨 0 몬스터
+Lv0 = images.Lv0_monster()
+#레벨 1 몬스터
+Lv1 = images.Lv1_monster()
+#레벨 2 몬스터
+Lv2 = images.Lv2_monster()
+#레벨 3 몬스터
+Lv3 = images.Lv3_monster()
+#레벨 4 몬스터
+Lv4 = images.Lv4_monster()
+#레벨 5 몬스터
+Lv5 = images.Lv5_monster()
+
 
 #화면 띄우기
 while True:
+    #이미지 변경
+    active_player = player[cur_color]
+    active_mirror = mirror[cur_color]
+    active_button = button[cur_color]
+    active_arrow = arrow[cur_color]
     #화면을 깨끗하게 한다.
     screen.fill((255,255,255)) #RGB
     
@@ -34,18 +59,19 @@ while True:
     screen.blit(images.ball_room, (0,730))
     screen.blit(images.card_room, (230,730))
     screen.blit(active_player, playpos)
-    screen.blit(images.zombie,(400,30))
-    screen.blit(images.zombie_barrier,(300,30))
-    screen.blit(images.nighthorror,(500,30))
-    screen.blit(images.ling,(600,30))
-    screen.blit(images.crawler,(700,30))
-    screen.blit(images.sting,(800,80))
-    screen.blit(images.cannon,(900,30))
-    screen.blit(images.ball,(900,40))
-    screen.blit(images.black,(500,310))
-    screen.blit(images.seer,(1200,30))
-    #screen.blit(images.final,(600,30))
-    screen.blit(images.card_Red, (400,735))
+    screen.blit(Lv1['zombie'],(400,30))
+    
+    # screen.blit(images.zombie_barrier,(300,30))
+    # screen.blit(images.nighthorror,(500,30))
+    # screen.blit(images.ling,(600,30))
+    # screen.blit(images.crawler,(700,30))
+    # screen.blit(images.sting,(800,80))
+    # screen.blit(images.cannon,(900,30))
+    # screen.blit(images.ball,(900,40))
+    # screen.blit(images.black,(500,310))
+    # screen.blit(images.seer,(1200,30))
+    # screen.blit(images.final,(600,30))
+    # screen.blit(images.card_Red, (400,735))
     screen.blit(active_button,(15,750))
    
 
@@ -71,23 +97,17 @@ while True:
                 keys[1] = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_1:
-                active_button = button[0]
-                active_player = player[0]
+                cur_color = 'red'
             elif event.key == pygame.K_2:
-                active_button = button[1]
-                active_player = player[1]
+                cur_color = 'green'
             elif event.key == pygame.K_3:
-                active_button = button[2]
-                active_player = player[2]
+                cur_color = 'blue'
             elif event.key == pygame.K_4:
-                active_button = button[3]
-                active_player = player[3]
+                cur_color = 'yellow'
             elif event.key == pygame.K_5:
-                active_button = button[4]
-                active_player = player[4]
+                cur_color = 'purple'
             elif event.key == pygame.K_6:
-                active_button = button[5]
-                active_player = player[5]
+                cur_color = 'jade'
         # if event.type == pygame.MOUSEBUTTONDOWN:
         #     position = pygame.mouse.get_pos()
         #     acc[1] = acc[1] + 1
